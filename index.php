ebcf7ec8b4e63779a241f0a4cf681e87e8006877
elharony <p.yahyaelharony@gmail.com>
2018-04-10 11:41:43 +0200

    Add static 'blog' section

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php get_header(); ?>
 
+
+<main>
+  <div class="container">
+    <!-- blog -->
+    <section class="blog">
+        <article>
+        <h3 class="title"><a href="#">Article Heading</a></h3>
+        <p class="brief"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit molestie egestas. Proin blandit, felis at rhoncus commodo, neque nulla molestie lorem, quis consectetur mauris eros efficitur felis. Aliquam pretium quis eros id condimentum.</p>
+        </article>
+        <article>
+        <h3 class="title"><a href="#">Article Heading</a></h3>
+        <p class="brief"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit molestie egestas. Proin blandit, felis at rhoncus commodo, neque nulla molestie lorem, quis consectetur mauris eros efficitur felis. Aliquam pretium quis eros id condimentum.</p>
+        </article>
+        <article>
+        <h3 class="title"><a href="#">Article Heading</a></h3>
+        <p class="brief"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit molestie egestas. Proin blandit, felis at rhoncus commodo, neque nulla molestie lorem, quis consectetur mauris eros efficitur felis. Aliquam pretium quis eros id condimentum.</p>
+        </article>
+    </section>
+  </div>
+</main>
+
+
 <?php get_footer(); ?>
