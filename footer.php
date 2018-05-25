2e8eeb6d93636002d3b59590eac9e363899483e4
elharony <p.yahyaelharony@gmail.com>
2018-05-25 05:19:39 +0200

    feat: Add simple 'footer' section

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
+        <footer>
+            <div class="container">
+                <h2 class="section-title">Reach me
+                    <span class="watermark">Contact</span>
+                </h2>
+                <ul class="contact">
+                    <li><a href="mailto:yahya@elharony.com">yahya@elharony.com</a></li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/elharony/"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://github.com/elharony"><i class="fab fa-github"></i></a>
+                    </li>
+                </ul>
+                <p class="copyrights">All copyrights reserved &copy; 2018</p>
+            </div>
+        </footer>
+     
      <?php wp_footer(); ?>
     </body>
 </html>
