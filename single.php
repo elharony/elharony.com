0e7c5e1b57a17f94d806119a590b7761e1dcca67
elharony <p.yahyaelharony@gmail.com>
2018-06-03 06:38:37 +0200

    feat: Add 'Reading Mode' in the article page

--- single.php
+++ single.php
@@ -18,6 +18,16 @@ the_post();
         </div>
     </section>
 
+    <div class="article-controls">
+        <div class="container">
+            <div class="reading-mode">
+                <button class="auto-scrolling-on show btn" title="Start Reading Mode"><i class="fas fa-book-open"></i></button>
+                <button class="auto-scrolling-off hide btn" title="Back to Normal"><i class="fas fa-eye"></i></button>
+            </div>
+        </div>
+    </div>
+    
+
     <article class="content">
         <div class="container">
             <?php the_content(); ?>
