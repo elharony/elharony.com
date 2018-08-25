71dc95a2fd7f4587527088c2b9be8d85923e8aae
elharony <p.yahyaelharony@gmail.com>
2018-08-25 09:15:58 +0200

    Add 'Ninja Form' shortcode with a wrapped div

--- footer.php
+++ footer.php
@@ -3,7 +3,10 @@
                 <h2 class="section-title">Reach me
                     <span class="watermark">Contact</span>
                 </h2>
-                <ul class="contact">
+                <div class="contact-form">
+                    <?php echo do_shortcode('[ninja_form id=3]'); ?>
+                </div>
+                <ul class="contact-links">
                     <li><a href="mailto:yahya@elharony.com">yahya@elharony.com</a></li>
                     <li>
                         <a href="https://www.linkedin.com/in/elharony/"><i class="fab fa-linkedin-in"></i></a>
