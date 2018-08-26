bfa53cee953e40e1560a1ccdaf9c5d3d6a0f8aee
elharony <p.yahyaelharony@gmail.com>
2018-08-26 07:31:07 +0200

    Add Facebook/YouTube links to the footer

--- footer.php
+++ footer.php
@@ -6,8 +6,10 @@
                 <ul class="contact-links">
                     <li><a href="mailto:yahya@elharony.com">yahya@elharony.com</a></li>
                     <li>
-                        <a href="https://www.linkedin.com/in/elharony/"><i class="fab fa-linkedin-in"></i></a>
-                        <a href="https://github.com/elharony"><i class="fab fa-github"></i></a>
+                        <a href="https://www.youtube.com/channel/UCcWSbBe_s-T_gZRnqFbtyIA"><i class="fab fa-youtube fa-fw"></i></a>
+                        <a href="https://www.linkedin.com/in/elharony/"><i class="fab fa-linkedin-in fa-fw"></i></a>
+                        <a href="https://github.com/elharony"><i class="fab fa-github fa-fw"></i></a>
+                        <a href="https://www.facebook.com/elharony"><i class="fab fa-facebook fa-fw"></i></a>
                     </li>
                 </ul>
                 <p class="copyrights">All copyrights reserved &copy; 2018</p>
