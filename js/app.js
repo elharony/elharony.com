0dbccbce9104bcca8fae479312ca530e383fe46e
elharony <p.yahyaelharony@gmail.com>
2018-11-10 23:21:37 +0200

    ui-ux: Comment the 'Reading Mode' feature - Expand the mobile view

--- js/app.js
+++ js/app.js
@@ -20,53 +20,53 @@
 
 
 
-/*
- * Automatic Scrolling
- */
-const autoScrollingOn = document.querySelector(".auto-scrolling-on");
-const autoScrollingOff = document.querySelector(".auto-scrolling-off");
-const moreSection = document.querySelector(".related-articles");
+// /*
+//  * Automatic Scrolling
+//  */
+// const autoScrollingOn = document.querySelector(".auto-scrolling-on");
+// const autoScrollingOff = document.querySelector(".auto-scrolling-off");
+// const moreSection = document.querySelector(".related-articles");
 
-// Interval
-let autoScrolling;
+// // Interval
+// let autoScrolling;
 
-// On/Off Switches
-autoScrollingOn.addEventListener("click", () => {
-    // Start Scrolling
-    autoScrollOn();
-    // Hide me
-    autoScrollingOn.classList.replace("show", "hide");
-    // Show the other Switch
-    autoScrollingOff.classList.replace("hide", "show");
-});
+// // On/Off Switches
+// autoScrollingOn.addEventListener("click", () => {
+//     // Start Scrolling
+//     autoScrollOn();
+//     // Hide me
+//     autoScrollingOn.classList.replace("show", "hide");
+//     // Show the other Switch
+//     autoScrollingOff.classList.replace("hide", "show");
+// });
 
-autoScrollingOff.addEventListener("click", () => {
-    // Start Scrolling
-    autoScrollOff();
-    // Hide me
-    autoScrollingOff.classList.replace("show", "hide");
-    // Show the other Switch
-    autoScrollingOn.classList.replace("hide", "show");
-});
+// autoScrollingOff.addEventListener("click", () => {
+//     // Start Scrolling
+//     autoScrollOff();
+//     // Hide me
+//     autoScrollingOff.classList.replace("show", "hide");
+//     // Show the other Switch
+//     autoScrollingOn.classList.replace("hide", "show");
+// });
 
-// Auto Scrolling [ON]
-function autoScrollOn() {
-    let scrollTop = window.scrollY;
-    autoScrolling = setInterval( () => {
-        scrollTop += 1;
-        window.scrollTo({
-            top: scrollTop,
-            behavior: "smooth"
-        })
+// // Auto Scrolling [ON]
+// function autoScrollOn() {
+//     let scrollTop = window.scrollY;
+//     autoScrolling = setInterval( () => {
+//         scrollTop += 1;
+//         window.scrollTo({
+//             top: scrollTop,
+//             behavior: "smooth"
+//         })
         
-        // Stop Scrolling if reaches the end of article
-        if(scrollTop > moreSection.offsetTop) {
-            clearInterval(autoScrolling);
-        }
-    }, 100);
-}
+//         // Stop Scrolling if reaches the end of article
+//         if(scrollTop > moreSection.offsetTop) {
+//             clearInterval(autoScrolling);
+//         }
+//     }, 100);
+// }
 
-// Auto Scrolling [OFF]
-function autoScrollOff() {
-    clearInterval(autoScrolling);
-}
+// // Auto Scrolling [OFF]
+// function autoScrollOff() {
+//     clearInterval(autoScrolling);
+// }
