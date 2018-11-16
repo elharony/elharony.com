bb482fbdedf1023a08a825bcabc929f0b0870ad8
elharony <p.yahyaelharony@gmail.com>
2018-11-16 08:07:06 +0200

    dev: Stop 'Google Analytics' from counting the visits from my 'localhost'

--- header.php
+++ header.php
@@ -5,11 +5,18 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128258295-1"></script>
 <script>
+
+// Don't count the Development Traffic!
+var host = window.location.hostname;
+if(host != "localhost")
+{
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
     gtag('config', 'UA-128258295-1');
+}
+    
 </script>
 
     <meta charset="<?php bloginfo("charset"); ?>">
