0a43be493b40aeded7eaed9f425dd71c285f15bb
elharony <p.yahyaelharony@gmail.com>
2018-11-26 04:48:33 +0200

    static: Change the header title of the 'contact' page

--- page-contact.php
+++ page-contact.php
@@ -4,7 +4,7 @@ get_header(); ?>
 <main class="page-contact">
     <section class="hero">
         <div class="container">
-            <h1 class="title">Contact Me</h1>
+            <h1 class="title">Reach Me</h1>
         </div>
     </section>
 
