b0284a8a2d48a812a2ff98759b63d7eed7f108fb
elharony <p.yahyaelharony@gmail.com>
2019-01-30 15:28:46 +0200

    Comment some of the footer's social media links

--- footer.php
+++ footer.php
@@ -9,10 +9,10 @@
                             <a href="https://www.youtube.com/channel/UCcWSbBe_s-T_gZRnqFbtyIA" title="Yahya Elharony on YouTube"><i class="fab fa-youtube fa-fw"></i></a>
                             <a href="https://www.linkedin.com/in/elharony/" title="Yahya Elharony on LinkedIn"><i class="fab fa-linkedin-in fa-fw"></i></a>
                             <a href="https://github.com/elharony" title="Yahya Elharony on Github"><i class="fab fa-github fa-fw"></i></a>
-                            <a href="https://www.facebook.com/elharony" title="Yahya Elharony on Facebook"><i class="fab fa-facebook fa-fw"></i></a>
+                            <!-- <a href="https://www.facebook.com/elharony" title="Yahya Elharony on Facebook"><i class="fab fa-facebook fa-fw"></i></a>
                             <a href="https://twitter.com/Yahya_Elharony" title="Yahya Elharony on Twitter"><i class="fab fa-twitter fa-fw"></i></a>
                             <a href="https://stackoverflow.com/users/5560399/elharony" title="Yahya Elharony on StackOverflow"><i class="fab fa-stack-overflow fa-fw"></i></a>
-                            <a href="https://medium.com/@elharony" title="Yahya Elharony on Medium"><i class="fab fa-medium fa-fw"></i></a>
+                            <a href="https://medium.com/@elharony" title="Yahya Elharony on Medium"><i class="fab fa-medium fa-fw"></i></a> -->
                         </li>
                     </ul>
                     <p class="copyrights">Beta Version - All copyrights reserved &copy; <?php echo date("Y"); ?></p>
