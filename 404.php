bea11aeccedbdaad1ca1073686f47c62c38405ac
elharony <yahya@elharony.com>
2019-05-24 10:36:14 +0200

    Add 'blog' section to '404' page

--- 404.php
+++ 404.php
@@ -11,4 +11,43 @@
             <a href="<?php echo get_home_url(); ?>" class="btn">Back to Homepage</a>
         </div>
     </section>
+
+    <!-- blog -->
+    <section class="blog">
+        <div class="container">
+            <h2 class="section-title">
+            Read my blog
+            <span class="watermark">Got a minute?</span>
+            </h2>
+            <div class="masonry">
+
+<?php
+
+$posts = get_posts(array(
+    'posts_per_page' => 6,
+    'orderby' => 'most_recent'
+));
+
+if ( $posts ) {
+    foreach ( $posts as $post ) :
+        setup_postdata( $post );
+?>
+    <article>
+        <div class="inner">
+            <div class="info"></i>Published on: <time><?php the_time('F j, Y'); ?></time></div>
+            <h3 class="title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+            <div class="info"></i>In: <span class="category"><?php the_category(' - '); ?></span></div>
+        </div>
+    </article>
+
+<?php
+    endforeach; 
+    wp_reset_postdata();
+}
+?>            
+
+            </div>
+        </div>
+	</section>
 </main>
+<?php get_footer(); ?>
