ce4e7b895ec07f99b16b58df1db0aa8aa04057a5
elharony <yahya@elharony.com>
2019-05-24 10:17:39 +0200

    Add '404' page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<main class="page-404 single">
+    <section class="hero">
+        <div class="container">
+            <h1 class="section-title">
+                Page Not Found
+                <span class="watermark">404 Error</span>
+            </h1>
+            <p>Sorry, the page you were looking for doesn't exist. It might have been removed, had its name changed, or is temporarily unavailable.</p>
+            <a href="<?php echo get_home_url(); ?>" class="btn">Back to Homepage</a>
+        </div>
+    </section>
+</main>
