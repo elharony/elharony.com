7ad39fb19d687d2af9e812b272544a955f622f93
elharony <yahya@elharony.com>
2019-07-20 06:53:01 +0200

    Show the 'Comments' again!

--- single.php
+++ single.php
@@ -43,18 +43,16 @@ the_post();
         </div>
     </article>
 
-<!--
     <section class="comments">
         <div class="container">
         <?php
         // If comments are open or we have at least one comment, load up the comment template.
-        // if ( comments_open() || get_comments_number() ) :
-        //     comments_template();
-        // endif;
+        if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif;
         ?>
         </div>
     </section>
--->
 
     
 <?php
