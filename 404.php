9d71caf6ac507c2ec816ecb011c956f4ef11644d
elharony <yahya@elharony.com>
2019-07-30 07:16:39 +0200

    Use the new 'blog' section on '404' page [Missed it before]

--- 404.php
+++ 404.php
@@ -3,10 +3,7 @@
 <main class="page-404 single">
     <section class="hero">
         <div class="container">
-            <h1 class="section-title">
-                Page Not Found
-                <span class="watermark">404 Error</span>
-            </h1>
+            <h1 class="section-title">Page Not Found</h1>
             <p>Sorry, the page you were looking for doesn't exist. It might have been removed, had its name changed, or is temporarily unavailable.</p>
             <a href="<?php echo get_home_url(); ?>" class="btn">Back to Homepage</a>
         </div>
@@ -15,11 +12,8 @@
     <!-- blog -->
     <section class="blog">
         <div class="container">
-            <h2 class="section-title">
-            Read my blog
-            <span class="watermark">Got a minute?</span>
-            </h2>
-            <div class="masonry">
+            <h2 class="section-title">Helpful Articles</h2>
+            <div class="blog-list">
 
 <?php
 
@@ -34,9 +28,20 @@ if ( $posts ) {
 ?>
     <article>
         <div class="inner">
-            <div class="info"></i>Published on: <time><?php the_time('F j, Y'); ?></time></div>
-            <h3 class="title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-            <div class="info"></i>In: <span class="category"><?php the_category(' - '); ?></span></div>
+            <div class="thumbnail">
+                <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
+                    <?php the_post_thumbnail('medium'); ?>
+                </a>
+            </div>
+            <div class="content">
+                <h3 class="title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+                <div class="brief"><?php the_excerpt() ?></div>
+                <?php the_category(''); ?>
+                <ul class="date">                    
+                    <li class="day"><?php the_time('j'); ?></li>
+                    <li class="month"><?php the_time('M'); ?></li>
+                </ul>
+            </div>
         </div>
     </article>
 
