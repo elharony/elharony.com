64272717241ac9d04d48778bcff713b20fd06579
elharony <yahya@elharony.com>
2019-08-19 06:27:09 +0200

    static: Update footer text

--- footer.php
+++ footer.php
@@ -1,27 +1,22 @@
         <footer>
-            <div class="footer-content">
-                <div class="container">
+            <div class="container">
+                <div class="footer-content">
+                    <p class="copyrights">Elharony &copy; <?php echo date("Y"); ?></p>
                     <ul class="contact-links">
                         <li>
                             <a href="https://www.youtube.com/channel/UCcWSbBe_s-T_gZRnqFbtyIA" title="Yahya Elharony on YouTube"><i class="fab fa-youtube fa-fw"></i></a>
                             <a href="https://www.linkedin.com/in/elharony/" title="Yahya Elharony on LinkedIn"><i class="fab fa-linkedin-in fa-fw"></i></a>
                             <a href="https://github.com/elharony" title="Yahya Elharony on Github"><i class="fab fa-github fa-fw"></i></a>
-                            <!-- <a href="https://www.facebook.com/elharony" title="Yahya Elharony on Facebook"><i class="fab fa-facebook fa-fw"></i></a>
-                            <a href="https://twitter.com/Yahya_Elharony" title="Yahya Elharony on Twitter"><i class="fab fa-twitter fa-fw"></i></a>
-                            <a href="https://stackoverflow.com/users/5560399/elharony" title="Yahya Elharony on StackOverflow"><i class="fab fa-stack-overflow fa-fw"></i></a>
-                            <a href="https://medium.com/@elharony" title="Yahya Elharony on Medium"><i class="fab fa-medium fa-fw"></i></a> -->
+                            <!-- <a href="https://www.facebook.com/elharony" title="Yahya Elharony on Facebook"><i class="fab fa-facebook fa-fw"></i></a> -->
+                            <!-- <a href="https://twitter.com/Yahya_Elharony" title="Yahya Elharony on Twitter"><i class="fab fa-twitter fa-fw"></i></a> -->
+                            <!-- <a href="https://stackoverflow.com/users/5560399/elharony" title="Yahya Elharony on StackOverflow"><i class="fab fa-stack-overflow fa-fw"></i></a> -->
+                            <!-- <a href="https://medium.com/@elharony" title="Yahya Elharony on Medium"><i class="fab fa-medium fa-fw"></i></a> -->
                         </li>
                     </ul>
-                    <p class="copyrights">Beta Version - All copyrights reserved &copy; <?php echo date("Y"); ?></p>
                 </div>
-            </div>
-                
-            <div class="disclaimer">
-                <strong>DISCLAIMER:</strong> Yahya Elharony is a Freelance Web Developer, he is not taking responsibility for any website he built for a client locally and/or globally. You are solely responsible for your own moves and decisions to use any website mentioned on this site, those websites aren't under the control of Yahya Elharony, including and without limitation: The nature of each site, Content, Payments, and the availability of their service!
             </div>
         </footer>
 
-        
      
      <?php wp_footer(); ?>
 
