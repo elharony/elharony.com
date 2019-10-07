f3adf8a2a28f86d75c519bc9b3e98458c2596c55
elharony <yahya@elharony.com>
2019-10-07 14:59:39 +0200

    ui-ux: Fix '404' page UI after changing the 'Hero' color

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
         <div class="container">
             <h1 class="section-title">Page Not Found</h1>
             <p>Sorry, the page you were looking for doesn't exist. It might have been removed, had its name changed, or is temporarily unavailable.</p>
-            <a href="<?php echo get_home_url(); ?>" class="btn">Back to Homepage</a>
+            <a href="<?php echo get_home_url(); ?>" class="btn btn-reversed">Back to Homepage</a>
         </div>
     </section>
 
