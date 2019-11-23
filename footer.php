a6c9bd5597e965b709262a3d480fce5b770e3cd7
elharony <yahya@elharony.com>
2019-11-23 19:40:20 +0200

    perf: Use 'Social Media Images' as SVG instead of Fontawesome Icons

--- footer.php
+++ footer.php
@@ -4,9 +4,16 @@
                     <p class="copyrights">Elharony &copy; <?php echo date("Y"); ?></p>
                     <ul class="contact-links">
                         <li>
-                            <a href="https://www.youtube.com/channel/UCcWSbBe_s-T_gZRnqFbtyIA" title="Yahya Elharony on YouTube"><i class="fab fa-youtube fa-fw"></i></a>
-                            <a href="https://www.linkedin.com/in/elharony/" title="Yahya Elharony on LinkedIn"><i class="fab fa-linkedin-in fa-fw"></i></a>
-                            <a href="https://github.com/elharony" title="Yahya Elharony on Github"><i class="fab fa-github fa-fw"></i></a>
+                            <a href="https://github.com/elharony" title="Yahya Elharony on Github">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>
+                            </a>
+                            <a href="https://www.linkedin.com/in/elharony/" title="Yahya Elharony on LinkedIn">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                            </a>
+                            <a href="https://www.youtube.com/channel/UCcWSbBe_s-T_gZRnqFbtyIA" title="Yahya Elharony on YouTube">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 9.333l5.333 2.662-5.333 2.672v-5.334zm14-4.333v14c0 2.761-2.238 5-5 5h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5h14c2.762 0 5 2.239 5 5zm-4 7c-.02-4.123-.323-5.7-2.923-5.877-2.403-.164-7.754-.163-10.153 0-2.598.177-2.904 1.747-2.924 5.877.02 4.123.323 5.7 2.923 5.877 2.399.163 7.75.164 10.153 0 2.598-.177 2.904-1.747 2.924-5.877z"/></svg>
+                            </a>
+
                             <!-- <a href="https://www.facebook.com/elharony" title="Yahya Elharony on Facebook"><i class="fab fa-facebook fa-fw"></i></a> -->
                             <!-- <a href="https://twitter.com/Yahya_Elharony" title="Yahya Elharony on Twitter"><i class="fab fa-twitter fa-fw"></i></a> -->
                             <!-- <a href="https://stackoverflow.com/users/5560399/elharony" title="Yahya Elharony on StackOverflow"><i class="fab fa-stack-overflow fa-fw"></i></a> -->
