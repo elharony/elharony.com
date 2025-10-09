dd50c2c4a20ac7661431d09049f48902e14b1d39
Yahya Elharony <yahya@elharony.com>
2025-10-09 14:06:22 +0300

    feat: integrate Netlify adapter for contact form
    - Added Netlify adapter to the Astro configuration for deployment.
    - Updated .gitignore to include the local Netlify folder.
    - Changed contact email from hello@elharony.com to yahya@elharony.com in the contact page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
+  output: 'server',
   site: 'https://elharony.com',
   integrations: [
     tailwind(),
